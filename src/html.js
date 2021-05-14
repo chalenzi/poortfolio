bb5e07ab8e72c92836ebcbc98d479b814ef1c9b5
chalenzi <chala021@gmail.com>
2021-05-14 22:56:51 +0200

    added photo

--- src/html.js
+++ src/html.js
@@ -19,6 +19,8 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="photo" content="gatsby-simplefolio\src\images\metal.jpg" />
+
         {headComponents}
       </head>
       <body {...bodyAttributes}>
