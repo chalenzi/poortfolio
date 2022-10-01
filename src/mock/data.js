c072858255011b318debdd8870bcbf5888520f54
“chalenzi” <chala021@gmail.com>
2022-10-01 14:39:53 +0200

    done

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'profile.png',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
